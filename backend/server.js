497a13da50ab1b62d01e5b401b1760efa809eacc
jayanta pal <paljayanta18@gmail.com>
2025-12-30 00:38:05 +0530

    add docker compose

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
-app.use(cors({
-  origin: "http://localhost:5173", // Vue dev server
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+      "http://116.202.104.108:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", userRoutes);
+app.use("/api", authRoutes);
 
-
-app.use('/api',userRoutes);
-app.use('/api',authRoutes);
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>console.log(`Server is Listening on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,"0.0.0.0", () =>
+  console.log(`Server is Listening on port ${PORT}`)
+);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "http://116.202.104.108:3000/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
