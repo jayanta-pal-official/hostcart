f517165f8df487478d0e9064fa8c5065377cdae5
jayanta pal <paljayanta18@gmail.com>
2026-01-12 01:05:41 +0530

    change base url for prod

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000/api",
-  baseURL: "http://116.202.104.108:3000/api",
+  baseURL: "http://116.202.104.108:3000/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
