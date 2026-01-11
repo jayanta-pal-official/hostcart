025d8cd19b70d9b4d66164fe2ebc0202dd9d40c5
jayanta pal <paljayanta18@gmail.com>
2026-01-12 01:10:35 +0530

    check database status

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-// import userRoutes from "./routes/user.route.js";
-// import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import db from "./db/db.js";
 const app = express();
 app.use(
   cors({
@@ -9,7 +10,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
-      "http://116.202.104.108",
+      "http://46.224.135.138",
     ],
     credentials: true,
   })
@@ -20,12 +21,21 @@ app.get("/", (req, res) => {
   res.send("Backend API is running 🚀");
 });
 
+app.get('/db-test', async (req, res) => {
+  try {
+    await db.query('SELECT 1');
+    res.json({ status: 'DB connected' });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 app.get('/api/health', (req, res) => {
   res.json({ status: 'Backend working' })
 })
 
-// app.use("/api", userRoutes);
-// app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,"0.0.0.0", () =>
